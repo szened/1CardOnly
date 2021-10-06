b0374cf9e703d1edcfde19059ba3dbab7cdfdf55
szened <szened@me.com>
2021-10-06 20:30:40 +0200

    001

---
+++ dist/main.js
@@ -0,0 +1,11 @@
+function $parcel$export(e, n, v, s) {
+  Object.defineProperty(e, n, {get: v, set: s, enumerable: true, configurable: true});
+}
+
+$parcel$export(module.exports, "add", () => $c79bbf7dbb0f3531$export$568ed88074851180);
+function $c79bbf7dbb0f3531$export$568ed88074851180(a, b) {
+    return a + b;
+}
+
+
+//# sourceMappingURL=main.js.map
---
+++ dist/module.js
@@ -0,0 +1,7 @@
+function $fd0e4966f38fc4e4$export$568ed88074851180(a, b) {
+    return a + b;
+}
+
+
+export {$fd0e4966f38fc4e4$export$568ed88074851180 as add};
+//# sourceMappingURL=module.js.map
